e7da478238cd1adb109229590270bfb951bda19d
seagate25 <sigidh.budi@gmail.com>
2022-07-24 22:19:38 +0700

    Add Menu Table Migration

---
+++ database/migrations/2022_07_24_143734_create_menus_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMenusTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('menus', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->unsignedBigInteger('parent')->nullable()->default(NULL);
+            $table->text('icon');
+            $table->string('uri');
+            $table->unsignedBigInteger('order');
+            $table->boolean('status')->default(true);
+            $table->string('created_by')->nullable();
+            $table->string('updated_by')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('menus');
+    }
+}
